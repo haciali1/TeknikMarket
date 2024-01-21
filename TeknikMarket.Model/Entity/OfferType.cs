da78d1f99e025e1058544367e8dbb73c393c69d7
Hacı Ali <116631053+haciali1@users.noreply.github.com>
2024-01-21 15:12:28 +0300

    Add project files.

---
+++ TeknikMarket.Model/Entity/OfferType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TeknikMarket.Model.Entity
+{
+    public partial class OfferType
+    {
+        public int? Id { get; set; }
+        public string? Type { get; set; }
+        public int? Creater { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public int? LastProcessUser { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public bool? IsDeleted { get; set; }
+
+        public virtual Offer? IdNavigation { get; set; }
+    }
+}
---
+++ TeknikMarket.Model/Entity/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TeknikMarket.Model.Entity
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            Baskets = new HashSet<Basket>();
+            InstallmentOptions = new HashSet<InstallmentOption>();
+            ProductImages = new HashSet<ProductImage>();
+            ProductPrices = new HashSet<ProductPrice>();
+        }
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Expllanation { get; set; }
+        public int? CategoriyId { get; set; }
+        public int? Stock { get; set; }
+        public int? Creater { get; set; }
+        public DateTime? CreaterDate { get; set; }
+        public int? LastProcessUser { get; set; }
+        public DateTime? UpdateData { get; set; }
+        public bool? IsDeleted { get; set; }
+
+        public virtual Category? Categoriy { get; set; }
+        public virtual ICollection<Basket> Baskets { get; set; }
+        public virtual ICollection<InstallmentOption> InstallmentOptions { get; set; }
+        public virtual ICollection<ProductImage> ProductImages { get; set; }
+        public virtual ICollection<ProductPrice> ProductPrices { get; set; }
+    }
+}
